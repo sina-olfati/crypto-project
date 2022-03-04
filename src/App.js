da35e06753d2edfb3fee9b9adaf7e674d2298e2c
sina <sinaolfati5@gmail.com>
2022-03-04 12:36:12 +0330

    API
    getting the information and passing it to our main file named "Landing.js" in "components" folder

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+// components
+import Landing from './components/Landing';
+
+function App() {
+  return (
+    <div>
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
