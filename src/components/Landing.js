47578c6ca129045c02170f42d09b2ec5c7cc3615
sina <sinaolfati5@gmail.com>
2022-03-04 13:53:21 +0330

    Loader
    i showed some details about coins and also added  a loading gif to it and shows it when the information is loading( with turnary operator).

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,13 +3,20 @@ import React, {useState, useEffect} from 'react'
 // API
 import { getCoin } from '../services/api'
 
+// components
+import Loader from './Loader'
+
 
 const Landing = () => {
+
+    const [coins, setCoins] = useState([])
+
 
     useEffect(() => {
         const fetchAPI = async () => {
             const data = await getCoin()
             console.log(data);
+            setCoins(data)
         }
         fetchAPI()
     }, [])
@@ -18,9 +25,19 @@ const Landing = () => {
 
 
   return (
-    <div>
-      Landing
-    </div>
+    <>
+        <input type="text" placeholder="Search" />
+        {
+          coins.length ?
+            <div>
+                {
+                    coins.map(item => <p key={item.id}><img src={item.image} />{item.id} / {item.symbol} / {item.current_price}</p>)
+                }
+            </div> : 
+            <Loader />
+        }
+
+    </>
   )
 }
 
